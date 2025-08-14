77b1431f70f4f0fc1c04235197a5e91950a5157d
Elina <elinamuhamedova13@gmail.com>
2025-08-14 15:59:56 +0500

    DFS and BFS

--- Search/maze.py
+++ Search/maze.py
@@ -60,7 +60,7 @@ class Maze():
 
         # Keep track of walls
         self.walls = []
-        for i in range(self.hight):
+        for i in range(self.height):
             row = []
             for j in range(self.width):
                 try:
@@ -69,6 +69,8 @@ class Maze():
                         row.append(False)
                     elif contents[i][j] == "B":
                         self.goal = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == " ":
                         row.append(False)
                     else:
                         row.append(True)
@@ -93,6 +95,7 @@ class Maze():
                     print("*", end="")
                 else:
                     print(" ", end="")
+            print()
         print()
     
     def neighbours(self, state):
@@ -100,5 +103,124 @@ class Maze():
         candidates = [
             ("up", (row - 1, col)),
             ("down", (row + 1, col)),
-            
+            ("left", (row, col - 1)),
+            ("right", (row, col + 1))
         ]
+
+        result = []
+        for action, (r, c) in candidates:
+            if 0 <= r < self.height and 0 <= c < self.width and not self.walls[r][c]:
+                result.append((action, (r, c)))
+        return result
+    
+    def solve(self):
+        """Finds a solution to maze, if one exists"""
+
+        # Keep track of number of states explored
+        self.num_explored = 0
+
+        # Initialise an empty explored set
+        self.explored = set()
+
+        # Initialise frontier to just the starting position
+        start = Node(state=self.start, parent=None, action=None)
+        frontier = QueueFrontier()
+        frontier.add(start)
+
+        # Keep looping until solution found
+        while True:
+
+            # If nothing left in frontier, then no path
+            if frontier.empty():
+                raise Exception("no solution")
+            
+            # Choose a node from the frontier
+            node = frontier.remove()
+            self.num_explored += 1
+
+            # If node is the goal, then we have a solution
+            if node.state == self.goal:
+                actions = []
+                cells = []
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+            
+            # Mark node as explored
+            self.explored.add(node.state)
+
+            # Add neighbours to frontier
+            for action, state in self.neighbours(node.state):
+                if not frontier.contains_state(state) and state not in self.explored:
+                    child = Node(state=state, parent=node, action=action)
+                    frontier.add(child)
+    
+    def output_image(self, filename, show_solution=True, show_explored=False):
+        from PIL import Image, ImageDraw
+        cell_size = 50
+        cell_border = 2
+
+        # Create a blank canvas
+        img = Image.new(
+            "RGBA",
+            (self.width * cell_size, self.height * cell_size),
+            "black"
+        )
+        draw = ImageDraw.Draw(img)
+
+        solution = self.solution[1] if self.solution is not None else None
+
+        for i, row in enumerate(self.walls):
+            for j, col in enumerate(row):
+
+                # Walls
+                if col:
+                    fill = (40, 40, 40)
+                
+                # Start
+                elif (i, j) == self.start:
+                    fill = (255, 0, 0)
+                
+                # Goal
+                elif (i, j) == self.goal:
+                    fill = (0, 171, 28)
+                
+                # Solution
+                elif solution is not None and show_solution and (i, j) in solution:
+                    fill = (220, 235, 113)
+                
+                # Explored
+                elif solution is not None and show_explored and (i, j) in self.explored:
+                    fill = (212, 97, 85)
+                
+                # Empty cell
+                else:
+                    fill = (237, 240, 252)
+                
+                # Draw cell
+                draw.rectangle(
+                    ([(j * cell_size + cell_border, i * cell_size + cell_border),
+                      ((j + 1) * cell_size - cell_border, (i + 1) * cell_size - cell_border)]),
+                    fill=fill
+                )
+
+        img.save(filename)
+
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python maze.py maze.txt")
+
+m = Maze(sys.argv[1])
+print("Maze:")
+m.print()
+print("Solving...")
+m.solve()
+print("States explored:", m.num_explored)
+print("Solution:")
+m.print()
+m.output_image("maze.png")
