a8df4b9b4ffe1d8c08dda0d9249b051d7d575c88
Elina <elinamuhamedova13@gmail.com>
2025-08-17 13:48:45 +0200

    model checking

---
+++ Knowledge/harry.py
@@ -0,0 +1,20 @@
+from logic import *
+
+rain = Symbol("rain")
+hagrid = Symbol("hagrid")
+dumbledore = Symbol("dumbledore")
+
+sentence = And(rain, hagrid)
+print(sentence.formula())
+
+knowledge = Implication(Not(rain), hagrid)
+print(knowledge.formula())
+
+knowledge = And(
+    Implication(Not(rain), hagrid),
+    Or(hagrid, dumbledore),
+    Not(And(hagrid, dumbledore)),
+    dumbledore
+)
+print(knowledge.formula())
+print(model_check(knowledge, rain))
